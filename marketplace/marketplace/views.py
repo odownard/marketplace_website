9834982afd8b55babaae64d6986a913fbf352354
odownard <downardo@stu.otc.school.nz>
2026-05-01 11:38:19 +1200

    Connected CSS to pages

--- marketplace/marketplace/views.py
+++ marketplace/marketplace/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello World! I'm Home.")
+#    return HttpResponse("Hello World! I'm Home.")
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse("My About Page.")
+#    return HttpResponse("My About Page.")
+    return render(request, 'about.html')
