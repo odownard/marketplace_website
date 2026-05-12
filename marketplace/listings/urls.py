11dd17864e9d078ea407fa5ea0d56d77466d5236
odownard <downardo@stu.otc.school.nz>
2026-05-13 10:39:45 +1200

    Individual Listings Pages Created

--- marketplace/listings/urls.py
+++ marketplace/listings/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'listings'
+
 urlpatterns = [
-    path('', views.listings_list),
+    path('', views.listings_list, name="list"),
+    path('<slug:slug>', views.listing_page, name="page"),
 ]
--- marketplace/listings/views.py
+++ marketplace/listings/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import listing
+from django.http import HttpResponse
 
 # Create your views here.
 def listings_list(request):
     listings = listing.objects.all().order_by('-date')
     return render(request, 'listings/listings.html', { 'listings': listings})
+
+def listing_page(request, slug):
+    return HttpResponse(slug)
