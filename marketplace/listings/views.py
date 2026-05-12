f46a59449aa3d8eff74e5028acabea347e2992de
odownard <downardo@stu.otc.school.nz>
2026-05-12 14:01:37 +1200

    Admin and listings created and displayed

--- marketplace/listings/views.py
+++ marketplace/listings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import listing
 
 # Create your views here.
 def listings_list(request):
-    return render(request, 'listings/listings.html')
+    listings = listing.objects.all().order_by('-date')
+    return render(request, 'listings/listings.html', { 'listings': listings})
