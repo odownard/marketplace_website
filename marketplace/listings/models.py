9641b176e3db8751d3254f5c1730cb8624ef134f
odownard <downardo@stu.otc.school.nz>
2026-05-15 11:42:41 +1200

    Images added to listing pages

--- marketplace/listings/models.py
+++ marketplace/listings/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class listing(models.Model):
+class Listing(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default='lambo.webp', blank=True)
     
     def __str__(self):
         return self.title
--- marketplace/listings/views.py
+++ marketplace/listings/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import listing
-from django.http import HttpResponse
+from .models import Listing
 
 # Create your views here.
 def listings_list(request):
-    listings = listing.objects.all().order_by('-date')
-    return render(request, 'listings/listings.html', { 'listings': listings})
+    listings = Listing.objects.all().order_by('-date')
+    return render(request, 'listings/listings.html', {'listings': listings})
 
 def listing_page(request, slug):
-    return HttpResponse(slug)
+    listing = Listing.objects.get(slug=slug)
+    return render(request, 'listings/listing_page.html', {'listing': listing})
