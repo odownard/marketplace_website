70cc222e8ee1c41e8ac7f4e50d163d0c696f3ca6
odownard <downardo@stu.otc.school.nz>
2026-06-18 15:06:42 +1200

    Added in location feild for the cards

--- marketplace/listings/models.py
+++ marketplace/listings/models.py
@@ -10,6 +10,27 @@ class Listing(models.Model):
     banner = models.ImageField(default='lambo.webp', blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     price = models.FloatField()
+
+    LOCATION_CHOICES = [
+        ('Northland', 'Northland'),
+        ('Auckland', 'Auckland'),
+        ('Waikato', 'Waikato'),
+        ('Bay Of Plenty', 'Bay Of Plenty'),
+        ('Gisborne', 'Gisborne'),
+        ("Hawke's Bay", "Hawke's Bay"),
+        ('Taranaki', 'Taranaki'),
+        ('Manawatū-Whanganui', 'Manawatū-Whanganui'),
+        ('Wellington', 'Wellington'),
+        ('Tasman', 'Tasman'),
+        ('Nelson', 'Nelson'),
+        ('Marlborough', 'Marlborough'),
+        ('West Coast', 'West Coast'),
+        ('Canterbury', 'Canterbury'),
+        ('Otago', 'Otago'),
+        ('Southland', 'Southland'),
+    ]
+
+    location = models.CharField(choices = LOCATION_CHOICES, null=True)
     
     def __str__(self):
         return self.title
