60bd56485b145711f399a9132332e3d7da8275b4
odownard <downardo@stu.otc.school.nz>
2026-07-23 15:10:48 +1200

    Added CSS to the user login and register pages

--- marketplace/users/forms.py
+++ marketplace/users/forms.py
@@ -1,3 +1,22 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
+from django.contrib.auth.models import User
+
+# class CustomRegistrationForm(UserCreationForm):
+#     class Meta(UserCreationForm):
+#         model = User
+#         fields = ('username', 'password')
+#         widgets = {
+#             'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+#             'password': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+#         }
+
+class CustomLoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={
+        'placeholder': 'Username',
+        'class': 'form-control' # Optional: Add CSS classes
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Password',
+        'class': 'form-control'
+    }))
--- marketplace/users/views.py
+++ marketplace/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import CustomLoginForm
 
 # Create your views here.
 def register_view(request):
@@ -15,7 +16,7 @@ def register_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = CustomLoginForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
             if 'next' in request.POST:
@@ -23,7 +24,7 @@ def login_view(request):
             else:
                 return redirect("listings:list")
     else:
-        form = AuthenticationForm()
+        form = CustomLoginForm()
     return render(request, "users/login.html", { "form": form })
 
 def logout_view(request):
