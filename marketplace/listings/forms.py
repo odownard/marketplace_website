febf2ec389a62ce3eb32a88bdafe82486acf764b
odownard <downardo@stu.otc.school.nz>
2026-08-05 13:49:22 +1200

    Styled the listing creation page

--- marketplace/listings/forms.py
+++ marketplace/listings/forms.py
@@ -5,3 +5,24 @@ class CreateListing(forms.ModelForm):
     class Meta:
         model = models.Listing
         fields = ['title', 'body', 'banner', 'price', 'location', 'category']
+
+        widgets ={
+            'title': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Name of Listing'
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-input',
+            }),
+            'body': forms.Textarea(attrs={
+                'class': 'form-input',
+                'placeholder': 'Description'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Price'
+            }),
+            'banner': forms.FileInput(attrs={
+                'class': 'form-input'
+            })
+        }
